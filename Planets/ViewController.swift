3e6f9406c7280850c3ac2252bc37c92332b14d17
John Carlisle <jmcarlisle@live.com>
2020-07-01 00:43:53 -0700

    Initial Commit

---
+++ Planets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Planets
+//
+//  Created by The Carlisle Family on 7/1/20.
+//  Copyright © 2020 The Carlisle Family. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
